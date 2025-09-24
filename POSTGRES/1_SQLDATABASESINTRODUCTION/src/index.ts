ed76668d703207b9e059348dfc4f760bc1d220f5
sumanitian <sumanprasad26042001@gmail.com>
2025-09-24 23:19:46 +0530

    credentials

--- POSTGRES/1_SQLDATABASESINTRODUCTION/src/index.ts
+++ POSTGRES/1_SQLDATABASESINTRODUCTION/src/index.ts
@@ -1,3 +1,21 @@
  import { Client } from "pg";
 
- const pgClient = new Client()
+  const pgClient = new Client("psql 'postgresql://neondb_owner:xxxxxxxxxxxxxxxx@ep-muddy-mouse-ad2urasj-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'")
+
+//  const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "xxxxxxxxxxxxxxxx",
+//     port: 5432,
+//     host: "ep-muddy-mouse-ad2urasj-pooler.c-2.us-east-1.aws.neon.tech",
+//     database: "neondb",
+
+//  })
+
+ async function main() {
+    await pgClient.connect();
+    const response = await pgClient.query("SELECT * FROM users;")
+    console.log(response);
+ }
+
+ main()
+ 
