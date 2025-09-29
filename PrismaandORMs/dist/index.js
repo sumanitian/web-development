2e14590754864d1a1edd66a0b0210239972f6f0f
sumanitian <sumanprasad26042001@gmail.com>
2025-09-29 18:37:55 +0530

    creating the first node app using prisma client

--- PrismaandORMs/dist/index.js
+++ PrismaandORMs/dist/index.js
@@ -1,4 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hii there");
+import { PrismaClient } from '@prisma/client';
+const client = new PrismaClient();
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "Suman",
+            password: "123123",
+            age: 23,
+            city: "Rourkela"
+        }
+    });
+}
+createUser();
 //# sourceMappingURL=index.js.map
--- PrismaandORMs/src/index.ts
+++ PrismaandORMs/src/index.ts
@@ -1,1 +1,43 @@
-console.log("hii there");
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient()
+
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "Suman",
+            password: "123123",
+            age: 23,
+            city : "Rourkela"
+        }
+    })
+    
+}
+createUser();
+
+async function deleteUser() {
+    await client.user.delete({
+        where: {
+            id: 1
+        }
+    })
+}
+deleteUser();
+
+async function updateUser() {
+    await client.user.update({
+        where: {
+            id: 2
+        },
+        data: {
+            city: "Bangalore"
+        }
+    })
+}
+updateUser();
+
+async function getUsers() {
+    const users = await client.user.findMany();
+    console.log(users);
+}
+getUsers
