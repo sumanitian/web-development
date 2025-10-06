c4e961631f89f1b8abd59fe184aa93e6235a1e3e
sumanitian <sumanprasad26042001@gmail.com>
2025-10-06 19:01:22 +0530

    issue in prisma, data is not being send to database

--- NextJS/fifthfullstackapplication/app/api/v1/signup/route.ts
+++ NextJS/fifthfullstackapplication/app/api/v1/signup/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
 
     try {
         // Creating a new user record in the database using Prisma
-        await prismaClient.user.create({
+        await prismaClient.User.create({
             data: {
                 username: data.username, // Storing the username
                 password: data.password  // Storing the password (Note: Ideally, passwords should be hashed before saving)
@@ -43,8 +43,8 @@ export async function POST(req: NextRequest) {
  */
 export async function GET() {
     // Retrieving the first user from the database
-    const user = await prismaClient.user.findFirst({});
+    const User = await prismaClient.User.findFirst({});
     
     // Returning the user's name and email in the response (Assuming email is stored in the username field)
-    return Response.json({ name: user?.username, email: user?.username });
+    return Response.json({ name: User?.username, email: User?.username });
 }
