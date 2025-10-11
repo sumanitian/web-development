19234e58e7387062b36d2239e6aa992bd7b3464b
sumanitian <sumanprasad26042001@gmail.com>
2025-10-11 19:35:31 +0530

    javascript foler

---
+++ JavaScritpt/Q1.js
@@ -0,0 +1,10 @@
+// Create a variable for each of the following: your favorite color, your height in centimeters, and whether you like pizza. Use appropriate variable declarations (let, const, or var). Try logging it using console.log
+
+const favCol = ["White", "Navy Blue", "Red"];
+const myHeight = "177.80 cm";
+const pizzaPerson = false;
+
+console.log(favColor);
+console.log(myHeight);
+console.log(pizzaPerson);
+
